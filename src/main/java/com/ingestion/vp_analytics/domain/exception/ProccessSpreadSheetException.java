66d2e7665df651bc1a50cab930324e2d3fda8b69
abnerwerley <abner.silva36@aluno.unip.br>
2026-03-04 21:08:46 -0300

    :rotating_light: fix: adding Exception custom class I forgot

---
+++ src/main/java/com/ingestion/vp_analytics/domain/exception/ProccessSpreadSheetException.java
@@ -0,0 +1,8 @@
+package com.ingestion.vp_analytics.domain.exception;
+
+public class ProccessSpreadSheetException extends RuntimeException {
+
+    public ProccessSpreadSheetException(String message) {
+        super(message);
+    }
+}
