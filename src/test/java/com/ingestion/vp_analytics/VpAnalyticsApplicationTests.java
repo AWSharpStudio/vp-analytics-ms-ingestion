82d9d78d40e4f3012f9084ef9a94ac04cfa4817b
abnerwerley <abner.silva36@aluno.unip.br>
2026-03-13 18:06:13 -0300

    :white_check_mark: fixed application test

--- src/test/java/com/ingestion/vp_analytics/VpAnalyticsApplicationTests.java
+++ src/test/java/com/ingestion/vp_analytics/VpAnalyticsApplicationTests.java
@@ -1,10 +1,16 @@
 package com.ingestion.vp_analytics;
 
+import com.ingestion.vp_analytics.adapters.input.web.IngestionController;
+import com.ingestion.vp_analytics.domain.ports.input.ProcessSpreadsheetInputPort;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@WebMvcTest
+@WebMvcTest(IngestionController.class)
 class VpAnalyticsApplicationTests {
+
+    @MockitoBean
+    private ProcessSpreadsheetInputPort useCase;
 
     @Test
     void contextLoads() {
